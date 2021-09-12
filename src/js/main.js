d3970a9ebae0b92e5ee222509912b96902da3327
Elmir Web <elmir.web@gmail.com>
2021-09-12 20:29:21 +0500

    Коммит четвертый. Полностью готовы страницы Техника, Подразделения, Комнаты

--- src/js/main.js
+++ src/js/main.js
@@ -55,17 +55,35 @@ window.onload = function () {
     .addEventListener("click", async (evt) => await CreateWorker(evt));
   // ____________________________________________________________ Сотрудники
 
-  // ____________________________________________________________ Сотрудники
+  // ____________________________________________________________ Паттерны сотрудников
   document
     .querySelector("#butt-create-unitpattern")
     .addEventListener("click", async (evt) => await CreateUnitPattern(evt));
-  // ____________________________________________________________ Сотрудники
+  // ____________________________________________________________ Паттерны сотрудников
 
-  // ____________________________________________________________ Сотрудники
+  // ____________________________________________________________ Типы техники
   document
     .querySelector("#butt-create-vktype")
     .addEventListener("click", async (evt) => await CreateVKType(evt));
-  // ____________________________________________________________ Сотрудники
+  // ____________________________________________________________ Типы техники
+
+  // ____________________________________________________________ Техника
+  document
+    .querySelector("#butt-create-vk")
+    .addEventListener("click", async (evt) => await CreateVK(evt));
+  // ____________________________________________________________ Техника
+
+  // ____________________________________________________________ Подразделения
+  document
+    .querySelector("#butt-create-unit")
+    .addEventListener("click", async (evt) => await CreateUnit(evt));
+  // ____________________________________________________________ Подразделения
+
+  // ____________________________________________________________ Комнаты
+  document
+    .querySelector("#butt-create-unitroom")
+    .addEventListener("click", async (evt) => await CreateUnitRoom(evt));
+  // ____________________________________________________________ Комнаты
 };
 
 function DisplayShowCondition(selectorCondition) {
@@ -164,7 +182,7 @@ async function CreateVKType(evt) {
       .execute(`insert into vktype (Name) values ('${vkType}')`);
   } else if (statusReadCreate == 1 /* изменить */) {
     if (idReadCreate == 0) {
-      window.alert("Не выбран сотрудник");
+      window.alert("Не выбран тип техники");
       return;
     }
     await remote
@@ -194,12 +212,12 @@ async function modeReadVKType(unitPatternID) {
     if (statusReadCreate == 0) {
       document.querySelector("#butt-create-vktype").innerHTML = "Создать";
       document.querySelector("#condition-vktype h2").innerHTML =
-        "Создать нового сотрудника";
+        "Создать новый тип";
     } else if (statusReadCreate == 1) {
       document.querySelector("#butt-create-vktype").innerHTML = "Изменить";
       document.querySelector(
         "#condition-vktype h2"
-      ).innerHTML = `Изменить сотрудника ID: ${unitPatternID}`;
+      ).innerHTML = `Изменить тип техники ID: ${unitPatternID}`;
     }
   }
 }
@@ -255,6 +273,28 @@ async function showVK() {
     .getGlobal("connectMySQL")
     .execute("select * from vktype");
 
+  for (
+    let i =
+      document.querySelector("select[name=FromVKTypeVKCrt]").options.length - 1;
+    i >= 0;
+    i--
+  ) {
+    document.querySelector("select[name=FromVKTypeVKCrt]").options[i] = null;
+  } // Очищаем выпадающие списки
+
+  if (rowsAllTypeVK.length) {
+    for (let j = 0; j < rowsAllTypeVK.length; j++) {
+      let newOption1 = new Option(
+        rowsAllTypeVK[j]["Name"],
+        rowsAllTypeVK[j]["ID"]
+      );
+
+      document.querySelector("select[name=FromVKTypeVKCrt]").options[
+        document.querySelector("select[name=FromVKTypeVKCrt]").length
+      ] = newOption1;
+    }
+  }
+
   let displayPrint = `
     <table style="font-size: 14px;">
       <thead>
@@ -274,7 +314,11 @@ async function showVK() {
     for (let i = 0; i < rowsVK.length; i++) {
       displayPrint += `
       <tr>
-        <td>${rowsVK[i]["ID"]}</td>
+        <td>${rowsVK[i]["ID"]} | <button onclick="DeleteVK(${
+        rowsVK[i]["ID"]
+      })">X</button><button onclick="modeReadVK(${
+        rowsVK[i]["ID"]
+      })">R</button></td>
         <td>${rowsVK[i]["Name"]}</td>
         <td>${rowsVK[i]["Model"]}</td>
         <td>${moment(rowsVK[i]["DatePurchase"]).format("YYYY-MM-DD")}</td>
@@ -298,6 +342,90 @@ async function showVK() {
 
   document.querySelector("#condition-vk .display-print").innerHTML =
     displayPrint;
+
+  document.querySelector("#butt-create-vk").innerHTML = "Создать";
+  statusReadCreate = 0;
+  document.querySelector("#condition-vk h2").innerHTML =
+    "Создать новую технику";
+}
+
+async function DeleteVK(vkID) {
+  const [rowsVKToUnit] = await remote
+    .getGlobal("connectMySQL")
+    .execute(`SELECT * FROM vktounit WHERE VKID = ${vkID}`);
+
+  if (rowsVKToUnit.length) {
+    for (let j = 0; j < rowsVKToUnit.length; j++) {
+      await remote
+        .getGlobal("connectMySQL")
+        .execute(`DELETE FROM vktounit WHERE ID = ${rowsVKToUnit[j]["ID"]}`);
+    }
+  }
+  await remote
+    .getGlobal("connectMySQL")
+    .execute(`DELETE FROM vk WHERE ID = ${vkID}`);
+
+  await showVK();
+}
+
+async function modeReadVK(vkID) {
+  if (idReadCreate !== vkID) {
+    idReadCreate = vkID;
+    statusReadCreate = 1;
+    document.querySelector("#butt-create-vk").innerHTML = "Изменить";
+    document.querySelector(
+      "#condition-vk h2"
+    ).innerHTML = `Изменить тип техники ID: ${vkID}`;
+  } else {
+    statusReadCreate = !statusReadCreate;
+    if (statusReadCreate == 0) {
+      document.querySelector("#butt-create-vk").innerHTML = "Создать";
+      document.querySelector("#condition-vk h2").innerHTML =
+        "Создать новую технику";
+    } else if (statusReadCreate == 1) {
+      document.querySelector("#butt-create-vk").innerHTML = "Изменить";
+      document.querySelector(
+        "#condition-vk h2"
+      ).innerHTML = `Изменить технику ID: ${vkID}`;
+    }
+  }
+}
+
+async function CreateVK(evt) {
+  evt.preventDefault();
+
+  let nameVK = document.querySelector("#input-name-vk-create").value;
+  let modelVK = document.querySelector("#input-model-vk-create").value;
+  let datePurchaseVK = document.querySelector(
+    "#input-DatePurchase-vk-create"
+  ).value;
+  let costVK = document.querySelector("#input-cost-vk-create").value;
+
+  let indexTypeVKVK = document.querySelector("select[name=FromVKTypeVKCrt]")
+    .options.selectedIndex;
+  let valueTypeVKVK = document.querySelector("select[name=FromVKTypeVKCrt]")
+    .options[indexTypeVKVK].value;
+
+  if (statusReadCreate == 0 /* создать */) {
+    await remote
+      .getGlobal("connectMySQL")
+      .execute(
+        `INSERT INTO vk (Name, Model, DatePurchase, Cost, TypeID) VALUES ('${nameVK}', '${modelVK}', '${datePurchaseVK}', '${costVK}', '${valueTypeVKVK}')`
+      );
+  } else if (statusReadCreate == 1 /* изменить */) {
+    if (idReadCreate == 0) {
+      window.alert("Не выбрана техника");
+      return;
+    }
+
+    await remote
+      .getGlobal("connectMySQL")
+      .execute(
+        `update vk set Name = '${nameVK}', Model = '${modelVK}', DatePurchase = '${datePurchaseVK}', Cost = '${costVK}', TypeID = '${valueTypeVKVK}' where id = ${idReadCreate}`
+      );
+  }
+
+  await showVK();
 }
 
 // показать сотрудников
@@ -445,7 +573,7 @@ async function showUnit() {
     for (let i = 0; i < rowsUnit.length; i++) {
       displayPrint += `
       <tr>
-        <td>${rowsUnit[i]["ID"]}</td>
+        <td>${rowsUnit[i]["ID"]} | <button onclick="DeleteUnit(${rowsUnit[i]["ID"]})">X</button><button onclick="modeReadUnit(${rowsUnit[i]["ID"]})">R</button></td>
         <td>${rowsUnit[i]["Number"]}</td>
         <td>${rowsUnit[i]["Name"]}</td>
         <td>${rowsUnit[i]["NameShort"]}</td>
@@ -467,6 +595,136 @@ async function showUnit() {
 
   document.querySelector("#condition-unit .display-print").innerHTML =
     displayPrint;
+
+  statusReadCreate = 0;
+
+  document.querySelector("#butt-create-unit").innerHTML = "Создать";
+  document.querySelector("#condition-unit h2").innerHTML =
+    "Создать новое подразделение";
+}
+
+async function CreateUnit(evt) {
+  evt.preventDefault();
+
+  let numberUnit = document.querySelector("#input-number-unit-create").value;
+  let nameUnit = document.querySelector("#input-name-unit-create").value;
+  let nameShortUnit = document.querySelector(
+    "#input-NameShort-unit-create"
+  ).value;
+
+  if (statusReadCreate == 0 /* создать */) {
+    await remote
+      .getGlobal("connectMySQL")
+      .execute(
+        `insert into unit (Number, Name, NameShort) values ('${numberUnit}', '${nameUnit}', '${nameShortUnit}')`
+      );
+  } else if (statusReadCreate == 1 /* изменить */) {
+    if (idReadCreate == 0) {
+      window.alert("Не выбрано подразделение");
+      return;
+    }
+    await remote
+      .getGlobal("connectMySQL")
+      .execute(
+        `update unit set Number = '${numberUnit}', Name = '${nameUnit}', NameShort = '${nameShortUnit}' where id = ${idReadCreate}`
+      );
+  }
+
+  await showUnit();
+}
+
+async function modeReadUnit(unitID) {
+  if (idReadCreate !== unitID) {
+    idReadCreate = unitID;
+
+    statusReadCreate = 1;
+
+    document.querySelector("#butt-create-unit").innerHTML = "Изменить";
+    document.querySelector(
+      "#condition-unit h2"
+    ).innerHTML = `Изменить подразделение ID: ${unitID}`;
+  } else {
+    statusReadCreate = !statusReadCreate;
+
+    if (statusReadCreate == 0) {
+      document.querySelector("#butt-create-unit").innerHTML = "Создать";
+      document.querySelector("#condition-unit h2").innerHTML =
+        "Создать новое подразделение";
+    } else if (statusReadCreate == 1) {
+      document.querySelector("#butt-create-unit").innerHTML = "Изменить";
+      document.querySelector(
+        "#condition-unit h2"
+      ).innerHTML = `Изменить подразделение ID: ${unitID}`;
+    }
+  }
+}
+
+async function DeleteUnit(unitID) {
+  const [rowsUnitPattern] = await remote
+    .getGlobal("connectMySQL")
+    .execute(`SELECT * FROM unitpattern WHERE UnitID = ${unitID}`);
+
+  if (rowsUnitPattern.length) {
+    let tempAllIDsUnitPattern = [];
+
+    for (let i = 0; i < rowsUnitPattern.length; i++) {
+      tempAllIDsUnitPattern.push(rowsUnitPattern[i]["ID"]);
+    }
+
+    for (let i = 0; i < tempAllIDsUnitPattern.length; i++) {
+      await remote
+        .getGlobal("connectMySQL")
+        .execute(
+          `DELETE FROM unitpattern WHERE ID = ${tempAllIDsUnitPattern[i]}`
+        );
+    }
+  }
+
+  const [rowsUnitRoom] = await remote
+    .getGlobal("connectMySQL")
+    .execute(`SELECT * FROM unitroom WHERE UnitID = ${unitID}`);
+
+  if (rowsUnitRoom.length) {
+    let tempAllIDsUnitRoom = [];
+
+    for (let i = 0; i < rowsUnitRoom.length; i++) {
+      tempAllIDsUnitRoom.push(rowsUnitRoom[i]["ID"]);
+    }
+
+    for (let i = 0; i < tempAllIDsUnitRoom.length; i++) {
+      const [rowsVKToUnit] = await remote
+        .getGlobal("connectMySQL")
+        .execute(
+          `SELECT * FROM vktounit WHERE RoomID = ${tempAllIDsUnitRoom[i]}`
+        );
+
+      if (rowsVKToUnit.length) {
+        let tempAllIDsVKToUnit = [];
+
+        for (let j = 0; j < rowsVKToUnit.length; j++) {
+          tempAllIDsVKToUnit.push(rowsVKToUnit[j]["ID"]);
+        }
+
+        for (let j = 0; j < tempAllIDsVKToUnit.length; j++) {
+          await remote
+            .getGlobal("connectMySQL")
+            .execute(
+              `DELETE FROM vktounit WHERE ID = ${tempAllIDsVKToUnit[j]}`
+            );
+        }
+      }
+
+      await remote
+        .getGlobal("connectMySQL")
+        .execute(`DELETE FROM unitroom WHERE ID = ${tempAllIDsUnitRoom[i]}`);
+    }
+  }
+
+  await remote
+    .getGlobal("connectMySQL")
+    .execute(`DELETE FROM unit WHERE ID = ${unitID}`);
+
+  await showUnit();
 }
 
 // показать принадлежность сотрудников
@@ -640,7 +898,7 @@ async function CreateUnitPattern(evt) {
       );
   } else if (statusReadCreate == 1 /* изменить */) {
     if (idReadCreate == 0) {
-      window.alert("Не выбран сотрудник");
+      window.alert("Не выбран паттерн сотрудников");
       return;
     }
 
@@ -670,7 +928,7 @@ async function modeReadUnitPattern(unitPatternID) {
     if (statusReadCreate == 0) {
       document.querySelector("#butt-create-unitpattern").innerHTML = "Создать";
       document.querySelector("#condition-unitpattern h2").innerHTML =
-        "Создать нового сотрудника";
+        "Создать новый паттерн сотрудника";
     } else if (statusReadCreate == 1) {
       document.querySelector("#butt-create-unitpattern").innerHTML = "Изменить";
       document.querySelector(
@@ -699,6 +957,26 @@ async function showUnitRoom() {
     .getGlobal("connectMySQL")
     .execute("select * from unit");
 
+  for (
+    let i =
+      document.querySelector("select[name=FromUnitUnitRoom]").options.length -
+      1;
+    i >= 0;
+    i--
+  ) {
+    document.querySelector("select[name=FromUnitUnitRoom]").options[i] = null;
+  } // Очищаем выпадающие списки
+
+  if (rowsAllUnit.length) {
+    for (let j = 0; j < rowsAllUnit.length; j++) {
+      let newOption1 = new Option(rowsAllUnit[j]["Name"], rowsAllUnit[j]["ID"]);
+
+      document.querySelector("select[name=FromUnitUnitRoom]").options[
+        document.querySelector("select[name=FromUnitUnitRoom]").length
+      ] = newOption1;
+    }
+  }
+
   let displayPrint = `
     <table style="font-size: 14px;">
       <thead>
@@ -722,7 +1000,7 @@ async function showUnitRoom() {
 
       displayPrint += `
         <tr>
-          <td>${rowsUnitRoom[i]["ID"]}</td>
+          <td>${rowsUnitRoom[i]["ID"]} | <button onclick="DeleteUnitRoom(${rowsUnitRoom[i]["ID"]})">X</button><button onclick="modeReadUnitRoom(${rowsUnitRoom[i]["ID"]})">R</button></td>
           <td>${tempUnitName}</td>
           <td>${rowsUnitRoom[i]["Name"]}</td>
         </tr>
@@ -743,6 +1021,91 @@ async function showUnitRoom() {
 
   document.querySelector("#condition-unitroom .display-print").innerHTML =
     displayPrint;
+
+  document.querySelector("#butt-create-unitroom").innerHTML = "Создать";
+  statusReadCreate = 0;
+  document.querySelector("#condition-unitroom h2").innerHTML =
+    "Создать новую комнату";
+}
+
+async function DeleteUnitRoom(unitroomID) {
+  const [rowsVKToUnit] = await remote
+    .getGlobal("connectMySQL")
+    .execute(`SELECT * FROM vktounit WHERE RoomID = ${unitroomID}`);
+
+  if (rowsVKToUnit.length) {
+    for (let j = 0; j < rowsVKToUnit.length; j++) {
+      await remote
+        .getGlobal("connectMySQL")
+        .execute(`DELETE FROM vktounit WHERE ID = ${rowsVKToUnit[j]["ID"]}`);
+    }
+  }
+  await remote
+    .getGlobal("connectMySQL")
+    .execute(`DELETE FROM unitroom WHERE ID = ${unitroomID}`);
+
+  await showUnitRoom();
+}
+
+async function modeReadUnitRoom(unitroomID) {
+  if (idReadCreate !== unitroomID) {
+    idReadCreate = unitroomID;
+
+    statusReadCreate = 1;
+
+    document.querySelector("#butt-create-unitroom").innerHTML = "Изменить";
+    document.querySelector(
+      "#condition-unitroom h2"
+    ).innerHTML = `Изменить комнату ID: ${unitroomID}`;
+  } else {
+    statusReadCreate = !statusReadCreate;
+    if (statusReadCreate == 0) {
+      document.querySelector("#butt-create-unitroom").innerHTML = "Создать";
+      document.querySelector("#condition-unitroom h2").innerHTML =
+        "Создать новую комнату";
+    } else if (statusReadCreate == 1) {
+      document.querySelector("#butt-create-unitroom").innerHTML = "Изменить";
+      document.querySelector(
+        "#condition-unitroom h2"
+      ).innerHTML = `Изменить комнату ID: ${unitroomID}`;
+    }
+  }
+}
+
+async function CreateUnitRoom(evt) {
+  evt.preventDefault();
+
+  let indexUnitUnitRoom = document.querySelector(
+    "select[name=FromUnitUnitRoom]"
+  ).options.selectedIndex;
+  let valueUnitUnitRoom = document.querySelector(
+    "select[name=FromUnitUnitRoom]"
+  ).options[indexUnitUnitRoom].value;
+
+  let nameUnitRoom = document.querySelector(
+    "#input-name-unitroom-create"
+  ).value;
+
+  if (statusReadCreate == 0 /* создать */) {
+    await remote
+      .getGlobal("connectMySQL")
+      .execute(
+        `INSERT INTO unitroom (UnitID, Name) VALUES ('${valueUnitUnitRoom}', '${nameUnitRoom}')`
+      );
+  } else if (statusReadCreate == 1 /* изменить */) {
+    if (idReadCreate == 0) {
+      window.alert("Не выбрана комната");
+      return;
+    }
+
+    await remote
+      .getGlobal("connectMySQL")
+      .execute(
+        `update unitroom set UnitID = '${valueUnitUnitRoom}', Name = '${nameUnitRoom}' where id = ${idReadCreate}`
+      );
+  }
+
+  await showUnitRoom();
 }
 
 // показать все перемещения техники
